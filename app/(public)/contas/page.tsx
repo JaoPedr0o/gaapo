2e4dd74600292b9ed1aaa5e4f8bdec2be2475f28
JãoPedr0o <joaopedromp15@outlook.com>
2026-03-15 22:23:06 -0300

    adicionar estrutura de pastas inicial

---
+++ app/(public)/contas/page.tsx
@@ -0,0 +1,8 @@
+
+export default function PaginaContas() {
+  return (
+    <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
+      <h1>Contas</h1>
+    </div>
+  );
+}
